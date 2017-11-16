4c8ccc045f25501460339e43fd04abc0a7b2fa17
Parthiban Subramanian <iamparthi007@gmail.com>
2017-11-16 10:58:15 +0530

    Initing

---
+++ server.js
@@ -0,0 +1,19 @@
+const app   = require('express')();
+const http  = require('http').Server(app);
+const io    = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection',(socket) => {
+    socket.on('send-message',(data) => {
+        io.emit('message-received',data);
+    });
+});
+
+http.listen(port, function(){
+  console.log('listening on *: ' + port);
+});
